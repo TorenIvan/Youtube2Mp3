dcd8be386fc8bd4cffd7b6c708c8da85a824a4fb
TorenIvan <Vaggelisshmos@gmail.com>
2020-12-25 01:00:31 +0200

    import module added, but switching protocols problem appeared...

--- js/script.js
+++ js/script.js
@@ -1,36 +1,28 @@
+import {validateYouTubeUrl} from '/js/validator.js';
+
 // Selecting elements from DOM
-const submit = document.querySelector('#submit');
-
-
-//check if youtube link is valid
-function validateYouTubeUrl(url) {    
-    if (url != undefined || url != '') {  
-        var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
-        var match = url.match(regExp);
-        if (match && match[2].length == 11) {
-            return true;         
-        } else {
-            return false;
-        }
-    }
-}
+var submit = document.querySelector('#submit');
 
 
 // AJAX
 submit.addEventListener('click', function(){   
     if(validateYouTubeUrl(document.querySelector('#url').value) == false){
-        // console.log('ee');
+        console.log('ee');
         return false;
     }
     let request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readyState == 4){
             if(request.status >= 200 && request.status < 300){
+                console.log("100");
                 //Get Response Here
+            }
+            else{
+                console.log("not");
             }
         }   
     };
 
-    request.open("POST", "file:///home/jyr/Documents/projects/Youtube2Mp3/index.html", true);
+    request.open("GET", "http://127.0.0.1:5500/index.html", true);
     request.send();
 });
