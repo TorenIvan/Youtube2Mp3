ae3d4ff4970ffa4133d925411247a5c792a14bb5
TorenIvan <Vaggelisshmos@gmail.com>
2020-12-24 19:37:27 +0200

    Validator Entered in js as well

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
+import {validateYouTubeUrl as validate} from 'validator.js';
+
 // Selecting elements from DOM
 const converter = document.querySelector('#converter');
 const url       = document.querySelector('#url');
 const submit    = document.querySelector('#submit');
+
+//check if youtube link is valid
+if(validate(url) === false){
+    alert(
+        'Enter a valid YouTube url. ' +
+        `${url} is not valid.`
+    );
+    return false;
+}
 
 // Manipulating style
 converter.style.color = '#0a4137';
@@ -15,7 +26,7 @@ submit.addEventListener('click', function(){
     const request = new XMLHttpRequest();
 
     request.onreadystatechange = function(){
-        if(request.readyState == 4) {
+        if(request.readyState == 4){
             if(request.status >= 200 && request.status < 300){
                 onLoadedFunc.call(null, request.responseText);
             }
