48f99f05276990a4fccd5bd30f9c629c20cfd36b
TorenIvan <Vaggelisshmos@gmail.com>
2020-12-26 22:14:11 +0200

    window redirecting may be the solution, commented the xhr

--- js/script.js
+++ js/script.js
@@ -10,31 +10,36 @@ submit.addEventListener('click', function(){
         console.log('ee');
         return false;
     }
-    var request = new XMLHttpRequest();
+    var url = document.querySelector('#url').value;
+    var path = "http://localhost:5005/song?url=" + url;
 
-    // The onreadystatechange property defines a function to be executed when the readyState changes.
-    request.onreadystatechange = function(){
-        // The readyState property holds the status of the XMLHttpRequest.
-        // 0   UNSENT               open() has not been called yet.
-        // 1   OPENED               send() has been called.
-        // 2   HEADERS_RECEIVED     send() has been called, and headers and status are available.
-        // 3   LOADING              Downloading; responseText holds partial data.
-        // 4   DONE                 The operation is complete.
-        if(request.readyState == 4){
-            console.log("perase to 4");
-            console.log("request.status: " + request.status);
-            // In local files, status is 0 upon success in Mozilla Firefox
-            if(request.status === 0 || (request.status >= 200 && request.status < 300)){
-                console.log("100");
-                //Get Response Here
-            }
-            else{
-                console.log("not");
-            }
-        }   
-    };
+    // var request = new XMLHttpRequest();
+    // request.open("GET", path, true);
+    // request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    
+    // // The onreadystatechange property defines a function to be executed when the readyState changes.
+    // request.onreadystatechange = function(){
+    //     // The readyState property holds the status of the XMLHttpRequest.
+    //     if(request.readyState == 4){
+    //         // In local files, status is 0 upon success in Mozilla Firefox
+    //         if(request.status == 200){
+    //             console.log("request");
+    //             console.log(request.status);
+    //             console.log(request.readyState);  
+    //             // console.log("Url: "+ url);
+    //             // console.log("Response: "+request.responseText);
+    //         }
+    //         else{
+    //             console.log("not");
+    //             console.log(request.status);
+    //         }
+    //     }   
+    // };
 
-    request.open("POST", "http://127.0.0.1:5500/index.html", true);
-    request.send();
+    window.location.href = path;
+
+
+    // request.send();
+    
 
 });
