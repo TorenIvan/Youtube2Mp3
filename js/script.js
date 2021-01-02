2c1233b57ff931931a3be3d19cf3d575bbff300a
TorenIvan <Vaggelisshmos@gmail.com>
2021-01-02 23:47:04 +0200

    Changing UI - adding filter buttons

--- js/script.js
+++ js/script.js
@@ -7,11 +7,10 @@ var submit = document.querySelector('#submit');
 // AJAX
 submit.addEventListener('click', function(){   
     if(validateYouTubeUrl(document.querySelector('#url').value) === false){
-        console.log('ee');
         return false;
     }
     var url = document.querySelector('#url').value;
-    var path = "http://127.0.0.1:5005/song?url=" + url;
+    var path = "http://127.0.0.1:5005/videos?url=" + url;
 
     window.open(path,"_blank");
 
