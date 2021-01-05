5f7e943571dec20a910f2e0820c6abc31eec38e5
TorenIvan <Vaggelisshmos@gmail.com>
2021-01-05 02:19:54 +0200

    request first try

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 import {validateYouTubeUrl} from '/js/validator.js';
 
 // Selecting elements from DOM
-var submit = document.querySelector('#submit');
-
+let submit = document.querySelector('#submit');
+let mp3Active = document.getElementById('mp3').className;
+let mp4Active = document.getElementById('mp4').className;
+let url = document.querySelector('#url');
+//Path of final youtube url
+var path;
 
 // AJAX
 submit.addEventListener('click', function(){   
-    if(validateYouTubeUrl(document.querySelector('#url').value) === false){
-        return false;
+    if(url.value && validateYouTubeUrl(url.value) === false){
+        return;
     }
-    var url = document.querySelector('#url').value;
-    var path = "http://localhost:5005/songs?url=" + url;
+
+    if(mp3Active =='active' && url.value){
+        path = "http://localhost:5005/songs?url=" + url.value;
+    }
+
+    if(mp4Active == 'active' && url.value){
+        path = "http://localhost:5005/videos?url=" + url.value;
+    }
 
     window.open(path,"_blank");
 
