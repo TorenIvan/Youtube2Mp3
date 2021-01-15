6e6826924aab0e2dca47874dfc366f7e89aaca0c
TorenIvan <Vaggelisshmos@gmail.com>
2021-01-16 00:48:32 +0200

    Processes decision making

--- private/server/processhandle/ytdlChildproc.js
+++ private/server/processhandle/ytdlChildproc.js
@@ -1,21 +1,21 @@
-// const YouTubeParser = require('../../youtubeclasses/youtubebasic');
+const YouTubeParser = require('../../youtubeclasses/youtubebasic');
 
-// //edo kanoume tous elegxous
-// function check(url){
+//Checking link
+function check(url){
 
-//   process.on('message', (m) => {
-//       console.log('CHILD got message:', m);
-//     });
+  process.on('message', (m) => {
+      console.log('CHILD got message:', m);
+    });
     
-//     // Causes the parent to print: PARENT got message: { foo: 'bar', baz: null }
-//     process.send({ foo: 'bar', baz: NaN });
+    // Causes the parent to print: PARENT got message: { foo: 'bar', baz: null }
+    process.send({ foo: 'bar', baz: NaN });
 
 
-//     // Child process will do the ytdl.getInfo and 
-//           //if not conditions => it will send a signal(or message) to father to exit 
-//           //else it will send a signal(or message) to father to pipe the mp3(or mp4 to the response)
+    // Child process will do the ytdl.getInfo and 
+          //if not conditions => it will send a signal(or message) to father to exit 
+          //else it will send a signal(or message) to father to pipe the mp3(or mp4 to the response)
 
-// }
+}
 
 
-// module.exports = check;
+module.exports = check;
--- private/server/processhandle/ytdlFatherproc.js
+++ private/server/processhandle/ytdlFatherproc.js
@@ -1,22 +1,21 @@
-// const fatherProc  =  require('child_process');
+const fatherProc  =  require('child_process');
 
-// const path = require('path');
-// const childProc   =  fatherProc.fork(path.join(__dirname, 'ytdlChildproc.js'), { silent: true });
+const path = require('path');
+const childProc   =  fatherProc.fork(path.join(__dirname, 'ytdlChildproc.js'), { silent: true });
 
 
-// //edo ftiaxnoume to mp3 ;i to mp4, type is mp3 or mp4
-// function build(url, type){
+function build(url, type){
 
-//   childProc.on('message', (m) => {
-//       console.log('PARENT got message:', m);
-//     });
+  childProc.on('message', (m) => {
+      console.log('PARENT got message:', m);
+    });
     
-//   // Causes the child to print: CHILD got message: { hello: 'world' }
-//   childProc.send({ hello: 'world' });
+  // Causes the child to print: CHILD got message: { hello: 'world' }
+  childProc.send({ hello: 'world' });
 
-//   // Father process will make the youtube download. If receives the message to kill it will be killed , otherwise continue
+  // Father process will make the youtube download. If receives the message to kill it will be killed , otherwise continue
 
-// }
+}
 
 
-// module.exports = build;
+module.exports = build;
